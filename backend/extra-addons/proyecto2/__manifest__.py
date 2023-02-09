5882c8a9ce4f129cefe46481de32f80087f526e5
unknown <javieralemanrodriguez@alumno.ieselrincon.es>
2023-02-09 17:24:56 +0000

    stages

--- backend/extra-addons/proyecto2/__manifest__.py
+++ backend/extra-addons/proyecto2/__manifest__.py
@@ -19,17 +19,19 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','project'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'data/project2_stages.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
+        
     ],
 
     'application': 'True',
--- backend/extra-addons/proyecto2/models/models.py
+++ backend/extra-addons/proyecto2/models/models.py
@@ -1,4 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
 class proyecto2(models.Model):
     _name = 'project.project'
     _inherit = 'project.project'
+
+    def _get_default_type_common(self):
+        ids = self.env["project.task.type"].search([("active", "=", True)])
+        return ids
+
+    type_ids = fields.Many2many(default=lambda self: self._get_default_type_common())
     
